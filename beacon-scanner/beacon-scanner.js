fe187b6bd04a9a929167cb20a5273e7b842b4754
sis0k0 <stanimira.vlaeva@gmail.com>
2016-10-01 12:23:39 +0300

    Add second distance formula, some measurements

--- beacon-scanner/beacon-scanner.js
+++ beacon-scanner/beacon-scanner.js
@@ -1,38 +1,26 @@
+'use strict';
+
 const fs = require('fs');
 
 const noble = require('noble');
 const jsonfile = require('jsonfile');
 
-const RSSI_THRESHOLD = -90;
-const EXIT_GRACE_PERIOD = 200; // milliseconds
 const FILENAME = './data2.json';
 
-let inRange = [];
-
 noble.on('discover', peripheral => {
-  if (peripheral.rssi < RSSI_THRESHOLD) {
-    return;
-  }
-
   let id = peripheral.id;
   let name = peripheral.advertisement && peripheral.advertisement.localName;
-  let entered = !inRange[id];
 
   if (typeof name !== 'undefined' && name.startsWith('Kontakt')) {
     let rssi = peripheral.rssi;
     let txPower = peripheral.advertisement.txPowerLevel;
     let distance = calculateDistance(rssi, txPower);
-    console.log(distance);
-
-    inRange[id] = {
-      peripheral: peripheral
-    };
 
     let beaconData = JSON.stringify({
       name,
       rssi,
       distance,
-      date: new Date()
+      date: Date.now()
     }, null, '\t');
 
     fs.appendFile(FILENAME, `${beaconData},\n`);
@@ -55,17 +43,16 @@ function calculateDistance(rssi, txPower) {
   }
 
   let ratio = rssi / txPower;
-  console.log(ratio);
 
   if (ratio < 1) {
     return Math.pow(ratio, 10);
   } else {
     let distance = (0.89976)*Math.pow(ratio,7.7095) + 0.111;
-    fs.appendFile('./distance.txt', `${rssi}\n`);
-    fs.appendFile('./time.txt', Date.now());
-    console.log(rssi);
-    console.log(Date.now());
     return distance / 1000000;
   }
 }
 
+function calculateDistance2(rssi, txPower) {
+  let distance = Math.pow(10, (txPower - rssi) / 20);
+  return distance / 1000;
+}

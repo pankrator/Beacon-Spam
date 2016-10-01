1e76beb2a04790e46557ee39675e10fa09ecce6b
sis0k0 <stanimira.vlaeva@gmail.com>
2016-10-01 21:54:43 +0300

    Implement config screen for nt app

--- listener/app/main-view-model.js
+++ listener/app/main-view-model.js
@@ -8,15 +8,15 @@ function getMessage(counter) {
     }
 }
 
-function createViewModel() {
+function createViewModel(onSubmitCallback) {
     var viewModel = new Observable();
     viewModel.counter = 42;
     viewModel.message = getMessage(viewModel.counter);
+    viewModel.listenerId = "Beacon_Alchohol"
+    viewModel.serverUrl = "http://10.255.255.16:8080/beacon/data"
+    viewModel.txPower = -12;
 
-    viewModel.onTap = function() {
-        this.counter--;
-        this.set("message", getMessage(this.counter));
-    }
+    viewModel.onSubmit = onSubmitCallback
 
     return viewModel;
 }
